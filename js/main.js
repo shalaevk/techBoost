c3e665e0c6358b0c7802a1ff493928cb3a96ade5
shalaevk <shalaevk1987@gmail.com>
2020-03-10 00:07:19 +0200

    jxjxmxmxm

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// $('#select').on('change', function() {
+//     var category = $(this).val();
+//     /* Hide all the divs */
+//     $('.option-grouping').hide();
+//     /* Now unhide the selected options */
+//     $('#' + category).show();
+// });
+
+// $('#select2').on('change', function() {
+//     var category = $(this).val();
+//     /* Hide all the divs */
+//     $('.strategy').hide();
+//     /* Now unhide the selected options */
+//     $('#' + category).show();
+// });
